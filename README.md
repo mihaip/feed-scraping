d8b8c8080a0737feb3a84e149f17c2546cde19ad
Mihai Parparita <mihaip@chromium.org>
2011-06-14 08:37:58 -0700

    Initial implementation.

---
+++ README.md
@@ -0,0 +1,10 @@
+GoComics.com (which recently merged with Comics.com) changed their RSS feeds to contain at best links only, and at worst links and ads, but no comic.
+
+This script fetches a GoComics.com feed URL and then for each item looks up the the actual comic image (conveniently emebdded in a `<link>` tag) and outputs a minimal Atom feed with the image. Sample usage:
+
+    python gocomics-scrape.py http://feeds.feedburner.com/uclick/frazz \
+        ~/www/scraped/frazz.xml
+
+I've put something like that in a cron job that runs once an hour.
+
+Incidentally, Frazz is the comic that I wanted this for, if you're looking for a full content feed for Frazz it can be found at [http://persistent.info/scraped/frazz.xml](http://persistent.info/scraped/frazz.xml).
---
+++ gocomics-scrape.py
@@ -0,0 +1,69 @@
+import formatter
+import htmllib
+import sys
+import urllib
+import xml.dom.minidom
+from xml.sax.saxutils import escape as xml_escape
+
+FEEDBURNER_NS = 'http://rssnamespace.org/feedburner/ext/1.0'
+XHTML_NS = 'http://www.w3.org/1999/xhtml'
+
+def open_url(url):
+  class Opener(urllib.FancyURLopener):
+      version = 'Mozilla/5.0 (compatible; Feedbot/1.0)'
+
+  return Opener().open(url)
+
+def get_feed_data(feed_url):
+  items = []
+  
+  feed_file = open_url(feed_url)
+  feed_dom = xml.dom.minidom.parse(feed_file)
+  
+  feed_title = feed_dom.getElementsByTagName('title')[0].firstChild.data
+  feed_title = feed_title.replace('GoComics.com - ', '')
+  
+  item_nodes = feed_dom.getElementsByTagName('item')
+  for item_node in item_nodes:
+    item_title = item_node.getElementsByTagName('title')[0].firstChild.data
+    item_link = item_node.getElementsByTagNameNS(FEEDBURNER_NS, 'origLink')[0].firstChild.data
+    items.append((item_title, item_link))
+  feed_file.close()
+  
+  return feed_title, items
+
+def get_item_image_url(item_url):
+  class ImageLinkParser(htmllib.HTMLParser):
+      def __init__(self):
+          htmllib.HTMLParser.__init__(self, formatter.NullFormatter())
+          self.image_url = None
+          
+      def do_link(self, attrs):
+        if ('rel', 'image_src') in attrs:
+          self.image_url = dict(attrs).get('href', None)
+
+  parser = ImageLinkParser()
+  item_file = open_url(item_url)
+  parser.feed(item_file.read())
+  parser.close()
+  item_file.close()
+  
+  return parser.image_url
+
+title, items = get_feed_data(sys.argv[1])
+
+print '<?xml version="1.0" encoding="utf-8"?>'
+print '<feed xmlns="http://www.w3.org/2005/Atom">'
+print '<title>%s</title>' % xml_escape(title)
+
+for item_title, item_url in items:
+  item_image_url = get_item_image_url(item_url)
+  print '<entry>'
+  print '  <title>%s</title>' % xml_escape(item_title)
+  print '  <link rel="alternate" href="%s" type="text/html"/>' % xml_escape(item_url)
+  print '  <content type="xhtml">'
+  print '    <div xmlns="%s"><img src="%s"/></div>' % (XHTML_NS, xml_escape(item_image_url))
+  print '  </content>' 
+  print '</entry>'
+
+print '</feed>'
