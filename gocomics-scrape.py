6e90990043d16ddfb1656ae51a2fe7a04bfbb748
Mihai Parparita <mihai@persistent.info>
2025-05-11 11:12:47 -0700

    Fix GoComics scraper again
    It turns out they have the strip image as an og:image tag, so we can just look for that.
    
    Also make the error state message only generate an item once a day, so that feed readers don't get flooded with messages.

--- gocomics-scrape.py
+++ gocomics-scrape.py
@@ -64,34 +64,14 @@ def get_strip_image_url(strip_url):
     def __init__(self):
       super().__init__()
       self.image_url = None
-      self.in_comic_viewer = False
-      self.tag_attrs_stack = []
-
-    def handle_starttag(self, tag, attrs):
-      self.tag_attrs_stack.append(attrs)
-      if self.is_comic_viewer(tag, attrs):
-        if self.in_comic_viewer:
-          logging.warning("Nested comic viewer divs found")
-        self.in_comic_viewer = True
-
-    def handle_endtag(self, tag):
-      attrs = self.tag_attrs_stack.pop()
-      if self.is_comic_viewer(tag, attrs):
-        self.in_comic_viewer = False
 
     def handle_startendtag(self, tag, attrs):
-      if self.in_comic_viewer and self.is_comic_image(tag, attrs):
-        if self.image_url:
-          logging.warning("Multiple comic image tags found")
-        self.image_url = dict(attrs).get('src', None)
-
-    def is_comic_viewer(self, tag, attrs):
-      class_name = dict(attrs).get('class', '')
-      return tag == 'div' and 'ComicViewer_comicViewer__comic' in class_name
-
-    def is_comic_image(self, tag, attrs):
-      class_name = dict(attrs).get('class', '')
-      return tag == 'img' and 'Comic_comic__image' in class_name
+      if tag == "meta":
+        attrs_dict = dict(attrs)
+        if attrs_dict.get('property') == 'og:image' and attrs_dict.get('content'):
+          if self.image_url:
+            logging.warning("Multiple image tags found")
+          self.image_url = attrs_dict["content"]
 
   parser = ImageParser()
   try:
@@ -136,7 +116,7 @@ for strip_date, strip_url in strips:
 if not strip_count:
   print('<entry>')
   print('  <title>Could not scrape feed</title>')
-  print('  <id>tag:persistent.info,2013:gocomics-scrape-%d</id>' % int(time.time()))
+  print('  <id>tag:persistent.info,2013:gocomics-scrape-error-%s</id>' % datetime.date.today().isoformat())
   print('  <link rel="alternate" href="https://github.com/mihaip/feed-scraping" type="text/html"/>')
   print('  <content type="html">')
   print('    Could not scrape the feed. Check the GitHub repository for updates.')
