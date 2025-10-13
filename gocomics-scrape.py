532a32d4f2672a3275840e330ac6854908506f0b
Mihai Parparita <mihai@persistent.info>
2025-10-12 20:28:40 -0700

    Update Go Comics scraper
    They have added https://bunny.net/shield/ and we need to specify more
    realistic HTTP headers to avoid being blocked.

--- gocomics-scrape.py
+++ gocomics-scrape.py
@@ -14,7 +14,20 @@ XHTML_NS = 'http://www.w3.org/1999/xhtml'
 def open_url(url):
   req = urllib.request.Request(
     url,
-    headers={'User-Agent': 'Mozilla/5.0 (compatible; Feedbot/1.0)'})
+    headers={
+      'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+      'accept-language': 'en-US,en;q=0.9',
+      'priority': 'u=0, i',
+      'sec-ch-ua': '"Google Chrome";v="141", "Not?A_Brand";v="8", "Chromium";v="141"',
+      'sec-ch-ua-mobile': '?0',
+      'sec-ch-ua-platform': '"macOS"',
+      'sec-fetch-dest': 'document',
+      'sec-fetch-mode': 'navigate',
+      'sec-fetch-site': 'none',
+      'sec-fetch-user': '?1',
+      'upgrade-insecure-requests': '1',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/141.0.0.0 Safari/537.36' ,
+    })
 
   return urllib.request.urlopen(req)
 
